2c817d5cea6b5517e5db805bc53d18602ca974da
Ladam0203 <adam7605@easv365.dk>
2022-03-06 16:20:17 +0100

    Can simulate game

--- src/dk/easv/bll/bot/UngaricheBot.java
+++ src/dk/easv/bll/bot/UngaricheBot.java
@@ -1,11 +1,16 @@
 package dk.easv.bll.bot;
 
+import dk.easv.bll.field.IField;
 import dk.easv.bll.game.GameManager;
 import dk.easv.bll.game.GameState;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
+import dk.easv.bll.move.Move;
 import dk.easv.gui.UTTTGameController;
 
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -19,30 +24,66 @@ public class UngaricheBot implements IBot{
 
     @Override
     public IMove doMove(IGameState state) {
+        /*
         if (tree.isEmpty())
         {
             tree.add(new Node(state));
             System.out.println("Root added");
         }
+        */
 
-        simulateRandomGame(state);
+        //here because if we mess with the state first, then the move choosing not works... cloning?
+        long time = System.currentTimeMillis();
+        int i = 0;
+        while (System.currentTimeMillis() - time < 999) //how does the time limit affect? if I put it over 1000ms it's still a valid move
+        {
+            simulateRandomGame(new GameState(state));
+            i++;
+        }
+        System.out.println("simulated games: " + i);
 
         List<IMove> moves = state.getField().getAvailableMoves();
+        IMove nextMove = null;
         if (moves.size() > 0) {
-            return moves.get(rnd.nextInt(moves.size())); /* get random move from available moves */
+            nextMove = moves.get(rnd.nextInt(moves.size())); /* get random move from available moves */
         }
-        return null;
+
+        return nextMove;
     }
 
     private boolean simulateRandomGame(IGameState state) //simulates a random game from a state, returns true if our bot wins
     {
-        GameManager gameManager = new GameManager(state, new RandomBot(), new RandomBot());
-
-        while (gameManager.getGameOver() == GameManager.GameOverState.Active
-                && gameManager.getCurrentState().getField().getAvailableMoves().size()>0) {
-            //simulate a random game
+        RandomBot randomBot = new RandomBot();
+        GameManager manager = new GameManager(state);
+        while (manager.getGameOver() == GameManager.GameOverState.Active)
+        {
+            IMove move = randomBot.doMove(manager.getCurrentState());
+            boolean isValid = verifyMoveLegality(state, move);
+            while(!isValid)
+            {
+                move = randomBot.doMove(manager.getCurrentState());
+                isValid = verifyMoveLegality(state, move);
+            }
+            manager.updateGame(randomBot.doMove(manager.getCurrentState()));
         }
-        return true;
+        //testing how to get if the bot or the enemy won
+        //System.out.println(manager.getGameOver().toString());
+        //System.out.println(manager.getCurrentPlayer() == 1);
+        return manager.getGameOver() == GameManager.GameOverState.Win && manager.getCurrentPlayer() == 1;
+    }
+
+    private Boolean verifyMoveLegality(IGameState state, IMove move)
+    {
+        IField field = state.getField();
+        boolean isValid=field.isInActiveMicroboard(move.getX(), move.getY());
+
+        if(isValid && (move.getX() < 0 || 9 <= move.getX())) isValid = false;
+        if(isValid && (move.getY() < 0 || 9 <= move.getY())) isValid = false;
+
+        if(isValid && !field.getBoard()[move.getX()][move.getY()].equals(IField.EMPTY_FIELD))
+            isValid=false;
+
+        return isValid;
     }
 
     @Override
