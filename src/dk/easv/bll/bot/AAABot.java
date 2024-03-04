b0771245257962012f74859200b64e4ba6bbfc47
Ádám Lőrincz <90683098+Ladam0203@users.noreply.github.com>
2024-03-04 22:47:53 +0100

    Only 2 nodes bruv

--- src/dk/easv/bll/bot/AAABot.java
+++ src/dk/easv/bll/bot/AAABot.java
@@ -5,6 +5,8 @@ import dk.easv.bll.game.GameState;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class AAABot implements IBot {
@@ -12,76 +14,141 @@ public class AAABot implements IBot {
 
     @Override
     public IMove doMove(IGameState state) {
-        IMove[] moves = state.getField().getAvailableMoves().toArray(new IMove[0]);
-
-        double[] w = new double[moves.length]; //no of simulations that resulted in a win for i
-        double[] s = new double[moves.length]; //no of simulations of i (visits)
-        double c = 1.414; //exploration parameter
-        double t = 0; //total number of simulations
-        double[] ucb = new double[moves.length]; //ucb value of i
-        for (int i = 0; i < moves.length; i++) {
-            ucb[i] = Double.MAX_VALUE;
-        }
+        Node root = new Node(null, state, null);
 
         long startTime = System.currentTimeMillis();
-        long endTime = startTime + state.getTimePerMove() - 900;
+        long endTime = startTime + state.getTimePerMove();
         while (System.currentTimeMillis() < endTime) {
-            // Select
-            int i = 0;
-            double ucbMax = -1;
-            for (int j = 0; j < moves.length; j++) {
-                if (s[j] < 2) { // Sim each node at least twice
-                    i = j;
-                    break;
-                }
-                if (ucb[j] > ucbMax) {
-                    ucbMax = ucb[j];
-                    i = j;
-                }
+            System.out.println("---Root node info---");
+            for (Node child : root.children) {
+                System.out.println(child.move + " " + child.s + " " + child.w + " " + child.getUCB());
             }
-
-            // Simulate
-            GameManager simulator = new GameManager(new GameState(state));
-            simulator.updateGame(moves[i]);
-            while (simulator.getGameOver() == GameManager.GameOverState.Active) {
-                IMove[] availableMoves = simulator.getCurrentState().getField().getAvailableMoves().toArray(new IMove[0]);
-                simulator.updateGame(availableMoves[r.nextInt(availableMoves.length)]);
-            }
-            s[i]++;
-            t++;
-
-            if (simulator.getGameOver() == GameManager.GameOverState.Win
-                    && simulator.getCurrentPlayer() == 1) {
-                w[i] += 1;
-            } else if (simulator.getGameOver() == GameManager.GameOverState.Tie) {
-                w[i] += 0.01;
-            }
-
-            // Backpropagate
-            double underSqrt = Math.log(t) / s[i];
-            double sqrt = Math.sqrt(underSqrt);
-            double cSqrt = c * sqrt;
-            double ws = w[i] / s[i];
-            ucb[i] = ws + cSqrt;
+            System.out.println("--------------------");
+            Node selected = select(root, 1);
+            Node expanded = expand(selected);
+            double result = simulate(expanded);
+            backpropagate(expanded, result);
         }
 
         //Find the best move (by highest no of simulations)
-        int bestMove = 0;
-        double sMax = -1;
-        for (int i = 0; i < moves.length; i++) {
-            System.out.printf("Move %d: %f/%f + %f * √(lg(%f)/%f) = %f\n", i, w[i], s[i], c, Math.log(t), s[i], ucb[i]);
-            if (s[i] > sMax) {
-                sMax = s[i];
-                bestMove = i;
+        Node best = root.children.getFirst();
+        for (Node child : root.children) {
+            System.out.println(child.move + " " + child.s + " " + child.w + " " + (double) child.w / child.s);
+            if (child.s > best.s) {
+                best = child;
             }
         }
+        System.out.println("Best move: " + best.move);
 
-        return moves[bestMove];
+        return best.move;
+    }
+
+    private Node select(Node node, int depth) {
+        if (node.children.isEmpty() || node.s < 2) {
+            System.out.println("Selected node at depth " + depth + " with " + node.s + " simulations");
+            return node;
+        }
+        Node best = node.children.getFirst();
+        for (Node child : node.children) {
+            if (child.getUCB() > best.getUCB()) {
+                best = child;
+            }
+        }
+        return select(best, ++depth);
+    }
+
+    private Node expand(Node node) {
+        if (node.availableMoves.isEmpty()) { //No more moves to expand
+            System.out.println("Tried to expand terminal node at depth " + getDepth(node));
+            return node;
+        }
+
+        //Create a new node with the next available move
+        GameManager simulator = new GameManager(new GameState(node.state));
+        IMove move = node.popMove();
+        simulator.updateGame(move);
+        Node newNode = new Node(move, simulator.getCurrentState(), node);
+        if (simulator.getGameOver() != GameManager.GameOverState.Active) {
+            //System.out.println("Terminal node: " + move + " at depth " + getDepth(node));
+            newNode.terminal = simulator.getCurrentPlayer(); //1 if win, 0 if tie/loss
+        }
+
+        node.children.add(newNode);
+        System.out.println("Expanded node at depth " + getDepth(node) + " with move " + move + " and " + newNode.availableMoves.size() + " available moves");
+
+        return newNode;
+    }
+
+    private double simulate(Node node) {
+        GameManager simulator = new GameManager(new GameState(node.state));
+
+        while (simulator.getGameOver() == GameManager.GameOverState.Active) {
+            IMove[] availableMoves = simulator.getCurrentState().getField().getAvailableMoves().toArray(new IMove[0]);
+            if (availableMoves.length == 0) {
+                return node.terminal;
+            }
+            simulator.updateGame(availableMoves[r.nextInt(availableMoves.length)]);
+        }
+        boolean win = simulator.getGameOver() == GameManager.GameOverState.Win
+                && simulator.getCurrentPlayer() == 1;
+
+        System.out.println("We simulated a " + (win ? "win" : "loss") + " at depth " + getDepth(node));
+        return win ? 1 : 0;
+    }
+
+    private void backpropagate(Node node, double result) {
+        node.w += result;
+        node.s++;
+        if (node.parent != null) {
+            backpropagate(node.parent, result);
+        }
     }
 
     @Override
     public String getBotName() {
         return "AAA Bot";
+    }
+
+    private int getDepth(Node node) {
+        int depth = 0;
+        while (node.parent != null) {
+            node = node.parent;
+            depth++;
+        }
+        return depth;
+    }
+}
+
+class Node {
+    IMove move;
+    IGameState state;
+    Node parent;
+    List<Node> children;
+    List<IMove> availableMoves;
+    double w;
+    double s;
+    int terminal;
+
+    public Node(IMove move, IGameState state, Node parent) {
+        this.move = move;
+        this.state = state;
+        this.parent = parent;
+        availableMoves = new LinkedList<>(state.getField().getAvailableMoves());
+        children = new LinkedList<>();
+        w = 0;
+        s = 0;
+        terminal = -1;
+    }
+
+    public double getUCB() {
+        if (s == 0) {
+            return Double.MAX_VALUE;
+        }
+        return w / s + 1.41 * Math.sqrt(Math.log(parent.s) / s);
+    }
+
+    public IMove popMove() {
+        return availableMoves.remove(0);
     }
 }
 
